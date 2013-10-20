a8286808ef844d9c4b73c077b3da01be07bc2575
Sebaci <sc.gman@gmail.com>
2013-10-20 18:42:04 +0200

    minor changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates_presence_of :title, :content, :category_id
 end
