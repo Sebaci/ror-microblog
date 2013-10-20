06c7922b3d07799c831e2f9b76a31cd7a359dfc0
Sebaci <sc.gman@gmail.com>
2013-10-20 15:58:51 +0200

    basic validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,13 +26,14 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comments = @post.comments
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @post, notice: 'Comment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @comment }
       else
-        format.html { render action: 'new' }
+        format.html { render 'posts/show' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
